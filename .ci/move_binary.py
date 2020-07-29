b09cc38fabc08a8bafc6ed6c943ca311cd6994e9
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2020-07-29 16:01:19 +0300

    Replace old binary

--- .ci/move_binary.py
+++ .ci/move_binary.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+
 import os
 import shutil
 import subprocess
@@ -13,8 +14,9 @@ demo_name = sys.argv[4]
 directory = f"demo_{demo_name}"
 
 os.chdir(master_repository_directory)
-
 filename = os.path.basename(binary_filename)
+
+# Include commit subject and hash to the new commit
 commit_hash = (
     subprocess.check_output(["git", "rev-parse", "--verify", "--short", "HEAD"])
     .decode("utf-8")
@@ -26,22 +28,6 @@ commit_subject = (
     .strip()
 )
 
-is_tag = env["GITHUB_EVENT_NAME"] == "push" and env["GITHUB_REF"].startswith(
-    "refs/tags"
-)
-if not is_tag:
-    directory = os.path.join(directory, "bin")
-    is_pr = env["GITHUB_REF"].startswith("refs/pull")
-    if is_pr:
-        # Pull Request - prN (pr1)
-        middle = "pr" + env["GITHUB_REF"].split("/")[2]
-        directory = os.path.join(directory, "prs")
-    else:
-        # Latest commit - short hash (20f2448)
-        middle = commit_hash
-    filename_split = filename.split("-")
-    filename = "-".join([*filename_split[:2], middle, *filename_split[2:]])
-
 # Set author info to the latest commit author
 author_name = subprocess.check_output(
     ["git", "log", "-1", "--pretty=format:%an"]
@@ -49,13 +35,53 @@ author_name = subprocess.check_output(
 author_email = subprocess.check_output(
     ["git", "log", "-1", "--pretty=format:%ae"]
 ).decode("utf-8")
-files_to_commit = []
 
-# Move file
 os.chdir(data_repository_directory)
 os.makedirs(directory, exist_ok=True)
+
+files_to_commit = []
+is_tag = env["GITHUB_EVENT_NAME"] == "push" and env["GITHUB_REF"].startswith(
+    "refs/tags"
+)
+is_pr = env["GITHUB_REF"].startswith("refs/pull")
+
+if not is_tag and is_pr:
+    # Pull Request - prN (pr1)
+    middle = "pr" + env["GITHUB_REF"].split("/")[2]
+    directory = os.path.join(directory, "prs")
+    filename_split = filename.split("-")
+    filename = "-".join([*filename_split[:2], middle, *filename_split[2:]])
+
+if not is_tag and not is_pr:
+    # Latest commit - short hash (20f2448)
+    middle = commit_hash
+    filename_split = filename.split("-")
+    filename = "-".join([*filename_split[:2], middle, *filename_split[2:]])
+
+    # Remove old commit binary
+    old_file = ""
+    for file in os.listdir(directory):
+        if (
+            not os.path.isfile(os.path.join(directory, file))
+            or not os.path.splitext(file)[1] == os.path.splitext(filename)[1]
+        ):
+            continue
+        file_split = file.split("-")
+        if (
+            len(file_split) != len(filename_split)
+            or filename_split[:2] + filename_split[2:]
+            != file_split[:2] + file_split[2:]
+        ):
+            continue  # Files must be in one format
+        if file_split[2].startswith("pr"):
+            continue  # Do not remove pr file
+        old_file = os.path.join(directory, file)
+        break
+    os.remove(os.path.join(directory, old_file),)
+    files_to_commit.append(os.path.join(directory, old_file))
+
 if is_tag:
-    # Move old release file
+    # Remove old release binary
     try:
         old_file = [
             file
@@ -63,13 +89,12 @@ if is_tag:
             if os.path.isfile(os.path.join(directory, file))
             and os.path.splitext(file)[1] == os.path.splitext(filename)[1]
         ][0]
-        shutil.move(
-            os.path.join(directory, old_file),
-            os.path.join(directory, "bin", old_file),
-        )
-        files_to_commit.append(os.path.join(directory, "bin", old_file))
+        os.remove(os.path.join(directory, old_file),)
+        files_to_commit.append(os.path.join(directory, old_file))
     except (IndexError, FileNotFoundError):
         pass
+
+# Add new binary
 shutil.copy(binary_filename, os.path.join(directory, filename))
 files_to_commit.append(os.path.join(directory, filename))
 
@@ -85,7 +110,13 @@ subprocess.check_call(
 )
 subprocess.check_call(["git", "push"])
 
+new_commit_hash = (
+    subprocess.check_output(["git", "rev-parse", "--verify", "--short", "HEAD"])
+    .decode("utf-8")
+    .strip()
+)
+
 print(
     f"Binary file: {env['GITHUB_SERVER_URL']}/{env['GITHUB_REPOSITORY']}/blob/"
-    f"{data_repository}/{directory}/{filename}"
+    f"{new_commit_hash}/{directory}/{filename}"
 )
